94c1d50a72122a3e25915622bec538618b643f9a
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-11-05 20:55:41 +0200

    added sections, css and js files

---
+++ src/js/refs.js
@@ -0,0 +1,12 @@
+// Обʼєкт refs з посиланнями на елементи ДОМ.
+
+const refs = {
+  categories: document.querySelector('.categories'),
+  products: document.querySelector('.products'),
+  modal: document.querySelector('.modal'),
+  modalProduct: document.querySelector('.modal-product'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+  loader: document.querySelector('.loader'),
+};
+
+export default refs;
