547c640481703c38aa618b1bcc28daabb97a8e8a
Nadiia Arshynova <nnarshinova@gmail.com>
2025-11-06 22:07:33 +0200

    faq-section_done

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,1 +1,10 @@
 // Функції для відображення елементів інтерфейсу.
+
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
+
+
+new Accordion('.accordion-container', {
+  duration: 300,
+  showMultiple: false,
+});
