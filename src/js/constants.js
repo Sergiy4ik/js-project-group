970263735e80db0b368d98da85a7804d6afc9270
Inna-Kmit <108284731+Inna-Kmit@users.noreply.github.com>
2025-11-06 17:06:38 -0800

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,1 +1,16 @@
 //API ендпоінти:
+
+export const BASE_URL = 'https://furniture-store-v2.b.goit.study/api';
+export const API_ENDPOINTS = {
+  FURNITURES: '/furnitures/',
+  CATEGORIES: '/categories',
+  FEEDBACKS: '/feedbacks',
+  ORDERS: '/orders',
+};
+export const FURNITURE_PAGE = 1;
+export const FURNITURE_LIMIT = 8;
+
+export const FEEDBACK_PAGE = 1;
+export const FEEDBACK_LIMIT = 10;
+
+export const getFurnitureByIdEndpoint = id => `/furnitures/${id}`;
