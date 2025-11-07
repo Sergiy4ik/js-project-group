944879e7af0d4b30b791f4d4e5f594259ee88dad
Inna-Kmit <108284731+Inna-Kmit@users.noreply.github.com>
2025-11-06 17:06:47 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 // Логіка сторінки index.html
+import { loadFurnitures } from './js/handlers.js';
+document.addEventListener('DOMContentLoaded', async () => {
+  console.log(' Перевірка API...');
+  const furnitures = await loadFurnitures({ page: 1, limit: 8 });
+  console.log('Меблі:', furnitures);
+});
