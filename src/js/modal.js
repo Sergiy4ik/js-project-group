9d42d2a573c81000cd1d508ad3c41ff810c66443
KateB713 <kbairachnaya@gmail.com>
2025-11-07 16:00:43 +0200

    add more js

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,10 +2,13 @@
 
 // order-modal
 
+const { default: iziToast } = require("izitoast");
+
 const backdropOrderModal = document.querySelector('.backdrop');
 const modalOrder = document.querySelector('.order-modal');
 const closeOrderBtn = document.querySelector('.modal-close-btn');
 const submitBtn = document.querySelector('.modal-submit-btn');
+const orderForm = document.querySelector('.modal-order-form');
 
 function openOrderModal() {
     backdropOrderModal.classList.add('is-open');
@@ -35,6 +38,35 @@ function handleEscape(event) {
     }
 }
 
+orderForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = orderForm.elements['user-name'].value.trim();
+    const phone = orderForm.elements['phone'].value.trim();
+    const comment = orderForm.elements['user-comment'].value.trim();
+
+    if (!name || !phone) {
+        iziToast.warning({
+            title: "Ooops!",
+            message: "Будь ласка, заповніть всі обов'язкові поля!",
+            position: "topRight",
+        });
+        return;
+    }
+
+    const clearPhone = phone.replace(/[^\d+]/g, '');
+    const phonePattern = /^\+?\d{10,15}$/;
+
+    if (!phonePattern.test(clearPhone)) {
+        iziToast.warning({
+            title: "Ooops!",
+            message: "Будь ласка, введіть коректний номер телефону",
+            position: "topRight",
+        });
+        return;
+    }
+    
+})
 
 // тестова кнопка для відкриття модалки
 document.querySelector('.open-modal-btn')?.addEventListener('click', openOrderModal);
