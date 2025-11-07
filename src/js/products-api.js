82d9b630599c74adab5eb87b46059c3ce80d0b93
Inna-Kmit <108284731+Inna-Kmit@users.noreply.github.com>
2025-11-06 17:06:45 -0800

    Update products-api.js

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,48 @@
 // Функції для запитів на бекенд.
+import axios from 'axios';
+import {
+  BASE_URL,
+  API_ENDPOINTS,
+  getFurnitureByIdEndpoint,
+  FURNITURE_PAGE,
+  FURNITURE_LIMIT,
+  FEEDBACK_PAGE,
+  FEEDBACK_LIMIT,
+} from './constants.js';
+axios.defaults.baseURL = BASE_URL;
+// Functions for Furnitures
+export async function getFurnitures(
+  params = { page: FURNITURE_PAGE, limit: FURNITURE_LIMIT }
+) {
+  const response = await axios.get(API_ENDPOINTS.FURNITURES, { params });
+  const furnituresData = response.data;
+  return furnituresData;
+}
+// Furniture by Id
+export async function getFurnitureByID(id) {
+  const endpoint = getFurnitureByIdEndpoint(id);
+  const response = await axios.get(endpoint);
+  const furnitureItem = response.data;
+  return furnitureItem; // один обєкт меблів
+}
+// Functions for Categories
+export async function getCategories() {
+  const response = await axios.get(API_ENDPOINTS.CATEGORIES);
+  const categoriesData = response.data;
+
+  return categoriesData; // масив категорій
+}
+// Feedbacks
+export async function getFeedbacks(
+  params = { page: FEEDBACK_PAGE, limit: FURNITURE_LIMIT }
+) {
+  const response = await axios.get(API_ENDPOINTS.FEEDBACKS, { params });
+  const feedbackData = response.data;
+  return feedbackData;
+}
+// Orders
+export async function createOrder(orderData) {
+  const response = await axios.post(API_ENDPOINTS.ORDERS, orderData);
+  const createOrder = response.data;
+  return createOrder;
+}
