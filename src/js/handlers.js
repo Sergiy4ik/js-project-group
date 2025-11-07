1621d4241836e894e8e3ee9c3e83cf5150396437
Inna-Kmit <108284731+Inna-Kmit@users.noreply.github.com>
2025-11-06 17:06:42 -0800

    Update handlers.js

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,1 +1,29 @@
 // Хендлери, які передаються в addEventListener.
+
+import {
+  getFurnitures,
+  getFurnitureByID,
+  getCategories,
+  getFeedbacks,
+  createOrder,
+} from './products-api.js';
+
+import {
+  FURNITURE_PAGE,
+  FURNITURE_LIMIT,
+  FEEDBACK_PAGE,
+  FEEDBACK_LIMIT,
+} from './constants.js';
+// furniture
+export async function loadFurnitures(
+  params = { page: FURNITURE_PAGE, limit: FURNITURE_LIMIT }
+) {
+  try {
+    const data = await getFurnitures(params);
+    console.log('Furnitures:', data);
+    return data;
+  } catch (error) {
+    console.error('Furnitures downloading error:', error);
+    return null;
+  }
+}
